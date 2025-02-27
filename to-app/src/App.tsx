13f7f8f3bd21d21301a36863cbf94077faac962d
Ignacia22 <mfernandezpolanco@gmail.com>
2025-02-27 18:36:57 -0300

    todo

---
+++ to-app/src/App.tsx
@@ -0,0 +1,58 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { JSX, useState } from "react";
+import { Todos } from "./components/Todos";
+import { type TodoId, type Todo as TodoType } from "./types";
+
+const mockTodos = [
+  {
+    id: '1',
+    title: 'Ignacia',
+    completed: true
+  },
+  {
+    id: '2',
+    title: 'todo 2',
+    completed: false
+  },
+  {
+    id: '3',
+    title: 'todo 3',
+    completed: false
+  }
+]
+
+const App = ():JSX.Element => {
+  const [todos, setTodos] = useState(mockTodos)
+
+  const handleRemove = ({id}: TodoId): void => {
+    const newTodos = todos.filter(todo => todo.id !== id)
+    setTodos(newTodos)
+  }
+
+  const handleCompleted = ({id, completed}: Pick<TodoType, 'id' | 'completed'>): void => {
+    const newTodos = todos.map(todo => {
+      if(todo.id === id) {
+        return {
+          ...todo,
+          completed
+        }
+      }
+      return todo
+    })
+    setTodos(newTodos)
+  }
+
+  return (
+    <div className="todoapp">
+      <Todos 
+      onToggleCompletedTodo={handleCompleted}
+      onRemoveTodo={handleRemove}
+      todos={todos}
+      />
+    </div>
+  )
+}
+
+
+export default App;
+
